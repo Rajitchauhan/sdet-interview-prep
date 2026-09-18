104b51bd71f0855d9cb7a427eca11f0f8bc42c87
Rajitchauhan <rajitkumarchauhan99@gmail.com>
2026-09-18 17:23:57 +0530

    complete ex 2

--- playwright/ex_2/test_product.py
+++ playwright/ex_2/test_product.py
@@ -8,4 +8,19 @@ def test_product_page(page: Page):
 
     expect(page.get_by_text("Dashboard")).to_be_visible()
 
-    
+    page.get_by_placeholder("Search").fill("iPhone 15")
+    page.get_by_role("button", name="Search").click()
+
+    product_card = page.locator(".product-card").filter(has_text="iPhone 15")
+
+    expect(product_card.locator(".product-name")).to_have_text("iPhone 15")
+    expect(product_card.locator(".product-price")).to_have_text("$799")
+
+    product_card.get_by_role("button", name="Add to Cart").click()
+
+    page.get_by_role("link", name="Shopping Cart").click()
+
+    product_items = page.locator(".cart-item").filter(has_text="iPhone 15")
+    expect(product_items.locator(".product-name")).to_have_text("iPhone 15")
+    expect(product_items.locator(".price")).to_have_text("$799")
+
